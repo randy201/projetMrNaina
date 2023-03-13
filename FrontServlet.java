9db9d737d4048808ebef35abd4d1639857e53188
randy201 <108166894+randy201@users.noreply.github.com>
2023-03-14 07:11:54 +0900

    Add files via upload

---
+++ FrontServlet.java
@@ -0,0 +1,21 @@
+package etu1989.framework.servlet;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.util.HashMap;
+
+public class FrontServlet extends HttpServlet{
+    
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+        out.println(req.getRequestURL());
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+        out.println(req.getRequestURL());
+
+    }
+
+}
